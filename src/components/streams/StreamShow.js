b0906c9efcb830c608c4cf54cbc09d02fa91d0c2
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-03-10 23:44:55 -0500

    Stream support added

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -2,19 +2,46 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions/index';
 import Spinner from '../Spinner';
+import flv from 'flv.js';
 
 class StreamShow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.videoRef = React.createRef();
+    }
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
+        this.buildPlayer();
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    componentWillUnmount() {
+        this.player.destroy();
+    }
+
+    buildPlayer() {
+        if (!this.props.stream || !this.videoRef) {
+            return;
+        }
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${this.props.match.params.id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render() {
-        const { title, description } = this.props.stream;
-        if (!this.props.stream) {
+        if (!this.props.stream || !this.videoRef) {
             return <Spinner />;
         }
+        const { title, description } = this.props.stream;
         return (
             <div>
+                <video ref={this.videoRef} style={{ width: '100%' }} controls />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
